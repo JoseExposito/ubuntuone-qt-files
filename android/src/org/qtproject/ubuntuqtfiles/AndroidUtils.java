27ef8789dc1c57a827eb69f90dcc41fbb9c3d311
José Expósito <jose.exposito89@gmail.com>
2013-12-05 17:26:42 +0000

    Fixed Android problems while open a file with special characters in the name

--- android/src/org/qtproject/ubuntuqtfiles/AndroidUtils.java
+++ android/src/org/qtproject/ubuntuqtfiles/AndroidUtils.java
@@ -29,9 +29,13 @@ public class AndroidUtils extends org.qtproject.qt5.android.bindings.QtActivity 
     }
 
     public static void openFile(String filePath) {
-        File file   = new File(filePath);
-        String ext  = MimeTypeMap.getFileExtensionFromUrl(file.toURI().toString());
-        String type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(ext);
+        File   file = new File(filePath);
+        String extension = "";
+        int i = filePath.lastIndexOf('.');
+        int p = Math.max(filePath.lastIndexOf('/'), filePath.lastIndexOf('\\'));
+        if (i > p)
+            extension = filePath.substring(i+1);
+        String type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
 
         if (type == null)
             type = "*/*";
